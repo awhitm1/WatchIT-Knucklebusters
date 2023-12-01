20d104dd54a5730013620222bc2ec14d59a1de35
awhitm1 <41929321+awhitm1@users.noreply.github.com>
2023-11-30 22:16:59 -0600

    basic list array and display

--- src/app/list-page/list-page.component.ts
+++ src/app/list-page/list-page.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Media } from './media.model';
+import { StreamInfo } from './streamInfo.model';
 
 @Component({
   selector: 'app-list-page',
   templateUrl: './list-page.component.html',
   styleUrls: ['./list-page.component.css']
 })
-export class ListPageComponent {
+export class ListPageComponent implements OnInit, OnDestroy{
+  myList: Media[] = [
+    new Media ('The Batman', 2022, new StreamInfo ('hbo', 'subscription', 'https://play.max.com/movie/dfa50804-e6f6-4fa2-a732-693dbc50527b', 'uhd'), 'tt1877830', 414906, 'movie' ),
+    new Media ('The Dark Knight Rises', 2012, new StreamInfo ('netflix', 'subscription', 'https://www.netflix.com/title/70213514/'), 'tt1345836', 49026, 'movie' ),
+    new Media ('Batman Forever', 1995, new StreamInfo ('hbo', 'subscription', 'https://play.max.com/movie/506c2994-fa03-452b-9131-e25d68fac01f', 'uhd'), 'tt1877830', 414, 'movie' ),
+    new Media ('Batman Returns', 1992, new StreamInfo ('hbo', 'subscription', 'https://play.max.com/movie/7ebb10fa-4552-405c-a5d2-3cc5b21193c7', 'uhd'), 'tt0103776', 364, 'movie' ),
+    new Media ('Batman', 1966, new StreamInfo ('hbo', 'subscription', 'https://play.max.com/movie/5e0da7f2-7422-4e58-8023-60bbd37adf33'), 'tt0060153', 2661, 'movie' )
+  ];
 
+  ngOnInit(): void {
+      console.log("myList: ", this.myList)
+  }
+
+  ngOnDestroy(): void {
+
+  }
 }
---
+++ src/app/list-page/media.model.ts
@@ -0,0 +1,12 @@
+import { StreamInfo } from "./streamInfo.model";
+
+export class Media {
+  constructor(
+    public title: string,
+    public year: number,
+    public service: StreamInfo,
+    public imdbId: string,
+    public tmdbId: number,
+    public type: string
+  ){}
+}
