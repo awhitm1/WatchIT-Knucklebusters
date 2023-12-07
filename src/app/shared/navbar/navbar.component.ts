e6e49db5ac1887211391e51b26e5176d872ae545
Neha Gupta <gupta.neha2004@gmail.com>
2023-12-07 15:19:51 -0800

    Responsive dark-theme navbar implemented

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import { AuthService } from 'src/app/auth/auth.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-
+  constructor(public authService: AuthService,private router: Router) { }
+  onLoginClick() {
+    this.authService.changeHasAccount(true);
+    this.router.navigate(['/auth']);
+  }
 }
