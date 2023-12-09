cf32106749872a159264b9de8bb7085ef2ed5661
Neha Gupta <gupta.neha2004@gmail.com>
2023-12-09 08:42:09 -0800

    Watchlist linked to list-page component

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
 import { AuthService } from 'src/app/auth/auth.service';
 import { Router } from '@angular/router';
+import { SearchService } from '../search.service';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-  constructor(public authService: AuthService,private router: Router) { }
+  searchTerm: string;
+  constructor(public authService: AuthService,private router: Router,private searchService: SearchService) { }
   onLoginClick() {
     this.authService.changeHasAccount(true);
     this.router.navigate(['/auth']);
   }
+  onSearchClick() {
+    this.searchService.searchMedia(this.searchTerm);
+    this.router.navigate(['/search-results']);
+}
 }
