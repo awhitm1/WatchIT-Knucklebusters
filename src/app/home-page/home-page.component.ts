53c852b27939f647dd539629dc211ea32fc95a13
splearn1 <barescents4u@gmail.com>
2023-12-09 15:12:38 -0500

    adjusting home page

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-home-page',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent {
+  userId: string;
+
+  constructor (private authService: AuthService) {}
+
+  ngOnInit() {
+    this.userId = localStorage.getItem('userId');
+  }
 
 }
