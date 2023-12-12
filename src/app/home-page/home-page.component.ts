28eb200209b09bbe78ff42f166d35febf6894ede
splearn1 <barescents4u@gmail.com>
2023-12-11 21:33:00 -0500

    added current user subscription name

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,18 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
+import { HttpClient } from '@angular/common/http';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
-export class HomePageComponent {
+
+export class HomePageComponent implements OnInit, OnDestroy {
   userId: string;
+  userSub: Subscription;
 
-  constructor (private authService: AuthService) {}
-
-  ngOnInit() {
-    this.userId = localStorage.getItem('userId');
+  constructor (private authService: AuthService, private http: HttpClient) {}
+  ngOnDestroy(): void {
+    this.userSub.unsubscribe();
   }
 
+  ngOnInit() {
+     this.userSub = this.authService.currentUser.subscribe((res) => {
+      this.userId = res.firstName;
+    });
+  }
 }
